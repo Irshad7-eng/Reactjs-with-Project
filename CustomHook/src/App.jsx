d1fe162550e3da3ddb23d2f96f39116ca2deb3c5
Irshad7.eng <mdiayan777@gmail.com>
2025-06-03 07:34:55 +0530

    this is custom hooks

---
+++ CustomHook/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState ,useCallback,useEffect, useRef} from 'react'
+import './App.css'
+
+function App() {
+
+
+  
+  return (
+    <  >
+
+      <div> Hello bor </div>
+    </>
+  )
+}
+
+export default App
